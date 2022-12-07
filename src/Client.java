967a25c0ef2ea95b5cbaf83c5eb3ff5c142750df
Дойчев Вячеслав <40699294+vdoichev@users.noreply.github.com>
2022-12-07 22:07:17 +0200

    Add dialog between client and server

--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) throws IOException {
         Socket clientSocket = new Socket("127.0.0.1", 8000);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        System.out.println(reader.readLine());
+
+        OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+
+        writer.write("Get me some information\n");
+        writer.flush();
+
+        String response = reader.readLine();
+        System.out.println(response);
+        writer.close();
+        reader.close();
         clientSocket.close();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,13 +12,20 @@ public class Server {
         while (true) {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client accepted #" + (++count));
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
             OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream());
-            writer.write("Client accepted #" + count);
+
+            String request = reader.readLine();
+            System.out.println(request);
+            String response = "#" + count + ", your message length is " + request.length() + "\n";
+            writer.write(response);
 //            writer.write("HTTP/1.0 200 ok\n" +
 //                    "Content-type: text/html\n" +"\n" +
 //                    "<h1>JAVA<h1>\n");
             writer.flush();
             writer.close();
+            reader.close();
             clientSocket.close();
         }
     }
